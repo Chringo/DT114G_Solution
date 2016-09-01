12f35f0f8cfa814d14b04d7d7d2e8f7be22d1080
Chringo <Chringo@users.noreply.github.com>
2016-09-01 19:04:20 +0200

    ADD Linked List
    Also move struct out of MonsterDB class

--- cppDatabaseProject/MonsterDatabase/MonsterDB.hpp
+++ cppDatabaseProject/MonsterDatabase/MonsterDB.hpp
@@ -2,30 +2,33 @@
 #define MONSTER_HPP
 #include <string>
 
-class MonsterDB
+struct Monster // TODO: Initiate before class but keep a limited scope
 {
 private:
-	// TODO: Linked list
+	std::string name,
+		race,
+		occupation;
+	int health;
+public:
+	Monster()
+	{
+		name = race = occupation = "Default";
+		health = -1;
+	}
+	Monster(std::string name_, std::string race_, std::string occupation_, int health_)
+	{
+		name = name_;
+		race = race_;
+		occupation = occupation_;
+		health = health_;
+	}
+};
+
+class MonsterDB
+{
 public:
 	MonsterDB();		// Initiate empty database
 	~MonsterDB();		// Deletes database
-
-	struct Monster // TODO: Initiate before class but keep a limited scope
-	{
-	private:
-		std::string name,
-			race,
-			occupation;
-		int health;
-	public:
-		Monster(std::string name_, std::string race_, std::string occupation_, int health_)
-		{
-			name = name_;
-			race = race_;
-			occupation = occupation_;
-			health = health_;
-		}
-	};
 
 	void add(const Monster& entry);			// Adds a monster to the database
 	Monster *find(const std::string name);	// Searches for THE monster with "name" and returns it. Returns NULL otherwise
@@ -34,5 +37,21 @@ public:
 	int size();								// Size of database, number of monsters
 	void print(Monster *entry);				// Prints out information of a monster
 	void printList();						// Prints out entire database of monsters
+private:
+	class Node
+	{
+	public:
+		Monster value;
+		Node* next;
+		Node(const Monster &value, Node* next = nullptr)
+		{
+			this->value = value;
+			this->next = next;
+		}
+		~Node() {}
+	};
+
+	Node* first;
+	int numNodes;
 };
 #endif
--- cppDatabaseProject/MonsterDatabase/main.cpp
+++ cppDatabaseProject/MonsterDatabase/main.cpp
@@ -5,16 +5,18 @@ using namespace std;
 
 int main()
 {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
 	MonsterDB db;
 
-	db.add(MonsterDB::Monster("Nisse", "Tomte", "Snickare", 5));
-	db.add(MonsterDB::Monster("Kisse", "Katt", "Musjagare", 2));
-	db.add(MonsterDB::Monster("Fnorg", "Bergstroll", "Chipsmumsare", 666));
+	db.add(Monster("Nisse", "Tomte", "Snickare", 5));
+	db.add(Monster("Kisse", "Katt", "Musjagare", 2));
+	db.add(Monster("Fnorg", "Bergstroll", "Chipsmumsare", 666));
 
 	db.printList();
 	cout << "List size: " << db.size() << endl;
 
-	MonsterDB::Monster *entry = db.find("Kisse");
+	Monster *entry = db.find("Kisse");
 	db.print(entry);
 	db.remove(entry);
 
